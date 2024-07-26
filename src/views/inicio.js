afd58739a95977549b547f7a972c4257e01d176c
APE Experience <151045379+APEexperience@users.noreply.github.com>
2024-07-26 15:45:59 -0400

    Update inicio.js

--- src/views/inicio.js
+++ src/views/inicio.js
@@ -636,7 +636,7 @@ const Inicio = (props) => {
           Suscr�bete a mi newsletter para poder recibir promociones y descuentos
         </p>
         <div className="inicio-content5">
-          <form onSubmit={handleSubmit} netlify>
+          <form onSubmit={handleSubmit}>
             <div className="inicio-inputs">
               <input
                 type="text"
