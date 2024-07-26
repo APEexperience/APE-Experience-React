6efbfc98399016cc604504d94f8c645a3bf78247
APE Experience <151045379+APEexperience@users.noreply.github.com>
2024-07-26 08:41:05 -0400

    Update newsletterSchema.js

--- models/newsletterSchema.js
+++ models/newsletterSchema.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-const newsletterSchema = new mongoose.Schema({
+const newsletterSchema = mongoose.Schema({
     name: String,
     email: String
 })
 
-const Newsletters = mongoose.model('Newsletters', newsletterSchema)
+const Newsletters = new mongoose.model('Newsletters', newsletterSchema)
 
 module.exports = Newsletters
