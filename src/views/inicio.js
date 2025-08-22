ec01d71790f7b6adf4b3b506190563b854bc59cb
APE Experience <151045379+APEexperience@users.noreply.github.com>
2025-08-22 00:38:57 -0400

    Update inicio.js

--- src/views/inicio.js
+++ src/views/inicio.js
@@ -7,7 +7,7 @@ import { Helmet } from 'react-helmet'
 
 import SectionNumeral from '../components/section-numeral'
 import BlogPostCard2 from '../components/blog-post-card2'
-import Newsletters from './Newsletters'
+import Newsletters from './api/newsletters'
 
 import './inicio.css'
 
