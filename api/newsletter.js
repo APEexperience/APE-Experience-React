0f7ca09a4ccb8af3a4a175204d2b2d4eca3a49fb
APE Experience <151045379+APEexperience@users.noreply.github.com>
2025-08-22 00:09:42 -0400

    Update newsletter.js

--- api/newsletter.js
+++ api/newsletter.js
@@ -1,1 +1,21 @@
+const fetchNewsletters = () => {
+  axios
+  .get('/api/Newsletters') // Cambia la URL absoluta por una relativa
+  .then((res) => {
+    console.log(res.data)
+  })
+}
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  axios
+  .post('./api/Newsletters', { name, email }) // Cambia la URL
+  .then(() => {
+    setName('')
+    setEmail('')
+    fetchNewsletters();
+  })
+  .catch((error) => {
+    console.log('No ha podido enviarse sus suscripción')
+  })
+}
